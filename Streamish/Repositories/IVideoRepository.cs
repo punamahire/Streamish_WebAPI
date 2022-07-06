272127dc9dc4f3e3a6626caf22b66e7be12c7bb3
Punam Ahire <punambahire@gmail.com>
2022-07-06 16:46:11 -0500

    Added code to make DB quesry based using query string params from the url

--- Streamish/Repositories/IVideoRepository.cs
+++ Streamish/Repositories/IVideoRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Streamish.Models;
+using System;
 using System.Collections.Generic;
 
 namespace Streamish.Repositories
@@ -12,5 +13,7 @@ namespace Streamish.Repositories
         void Update(Video video);
         List<Video> GetAllWithComments();
         Video GetVideoByIdWithComments(int id);
+        List<Video> Search(string criterion, bool sortDescending);
+        List<Video> Hottest(DateTime dt);
     }
 }
