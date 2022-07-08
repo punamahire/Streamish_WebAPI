4515c9f27ab4578219f57e742f2cb8a3ae82dbf6
Punam Ahire <punambahire@gmail.com>
2022-07-08 09:41:38 -0500

    added routing for react components

--- Streamish/client/src/components/VideoForm.js
+++ Streamish/client/src/components/VideoForm.js
@@ -1,75 +1,58 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 import { addVideo } from "../modules/videoManager";
 
 const VideoForm = ({ getVideos }) => {
+    const emptyVideo = {
+        title: '',
+        description: '',
+        url: ''
+    };
 
-    const [video, setVideo] = useState({
-        title: "",
-        description: "",
-        url: "",
-    });
+    const [video, setVideo] = useState(emptyVideo);
 
-    const [isLoading, setIsLoading] = useState(true);
+    const handleInputChange = (evt) => {
+        const value = evt.target.value;
+        const key = evt.target.id;
 
-    //when a field changes, update state. The return will re-render and display based on the values in state
-    //Controlled component
-    const handleControlledInputChange = (event) => {
-        /* When changing a state object or array,
-        always create a copy, make changes, and then set state.*/
-        const newVideo = { ...video }
-        let selectedVal = event.target.value
+        const videoCopy = { ...video };
 
-        newVideo[event.target.id] = selectedVal
-        // update state
-        setVideo(newVideo);
-    }
+        videoCopy[key] = value;
+        setVideo(videoCopy);
+    };
 
-    useEffect(() => {
-        setIsLoading(false);
-    }, []);
+    const handleSave = (evt) => {
+        evt.preventDefault();
 
-    const handleClickAddVideo = (event) => {
-        event.preventDefault() //Prevents the browser from submitting the form
-
-        if (video.title === "" || video.url == "") {
-            window.alert("Video title and url cannot be blank")
-        } else {
-            // using addVideo invoke the API to store new video to DB.
-            // once complete, refresh the list to display the added video.
-            setIsLoading(true);
-            addVideo(video)
-                .then(() => {
-                    console.log("Video added");
-                    getVideos();
-                });
-        }
-    }
+        addVideo(video).then(() => {
+            setVideo(emptyVideo);
+            getVideos();
+        });
+    };
 
     return (
-        <form className="videoForm col-sm-6">
-            <h2 className="videoForm__title">New Video</h2>
-            <fieldset>
-                <div className="form-group mb-2">
-                    <input type="text" id="title" onChange={handleControlledInputChange} required autoFocus className="form-control" placeholder="Video title" value={video.title} />
-                </div>
-            </fieldset>
-            <fieldset>
-                <div className="form-group mb-2">
-                    <input type="text" id="description" onChange={handleControlledInputChange} required autoFocus className="form-control" placeholder="Video Description" value={video.description} />
-                </div>
-            </fieldset>
-            <fieldset>
-                <div className="form-group mb-2">
-                    <input type="text" id="url" onChange={handleControlledInputChange} required autoFocus className="form-control" placeholder="Video url" value={video.url} />
-                </div>
-            </fieldset>
-            <button type="button" className="btn btn-primary"
-                disabled={isLoading}
-                onClick={handleClickAddVideo}>
-                Add Video
-            </button>
-        </form>
-    )
+        <Form>
+            <FormGroup>
+                <Label for="title">Title</Label>
+                <Input type="text" name="title" id="title" placeholder="video title"
+                    value={video.title}
+                    onChange={handleInputChange} />
+            </FormGroup>
+            <FormGroup>
+                <Label for="url">URL</Label>
+                <Input type="text" name="url" id="url" placeholder="video link"
+                    value={video.url}
+                    onChange={handleInputChange} />
+            </FormGroup>
+            <FormGroup>
+                <Label for="description">Description</Label>
+                <Input type="textarea" name="description" id="description"
+                    value={video.description}
+                    onChange={handleInputChange} />
+            </FormGroup>
+            <Button className="btn btn-primary" onClick={handleSave}>Submit</Button>
+        </Form>
+    );
 };
 
 export default VideoForm;
